fdb12883c9c0be40503f7e33a0e119d7280461e7
Smoodie <89829273+SwaggySmoodie@users.noreply.github.com>
2023-03-07 18:10:43 +0100

    Optimizing code

--- no_gui_version.py
+++ no_gui_version.py
@@ -1,60 +1,49 @@
-#  BY SMOODIE
+# BY SMOODIE
 from tkinter import *
 from tkinter import filedialog
 from pathlib import Path
-import time
 
-#  All important variables here to simplify reading my code
-directory = None
-path = None
-save = None
-room = None
-
-
-def main():  # Main function
+def main():
+    # ASCII GUI
     print('------------------------------\n'
           '| SPOOKY EDITOR / BY SMOODIE |\n'
           '------------------------------\n')
     print("\n[#] Select your game directory (run the gui version for a tutorial ) :")
-    tk = Tk()
-    time.sleep(1)
-    tk.geometry("0x0")
-    tk.wm_attributes('-toolwindow', 'True')
-    global directory
+
+    # Ask for a directory
     directory = filedialog.askdirectory(title="Select the game directory...")
-    path = Path(f"{directory}/save_data_main.ini")
 
-    if path.is_file():  # If save file exist
-        with open(path, 'r') as save:
-            lines = save.readlines()
-        print("\n[*] Your currently at the", lines[5])
-        print("[!] Please choose a number superior than 0 and inferior than 1000!")
-        room = input("[#] Change the room value : ")
-    #   if room < '0' or room > '1000':
-    #        print("[!] ERROR: You can't go to a negative or superior than 1000 room!")
-    #        input("[*] Press Enter to continue...")
-    #        exit()
-    #   else:        (i need to convert str into int but idk how so nvm)
-        lines[5] = f"room={str(room)}" + "\n"
-        with open(path, 'w+') as save:
-            save.writelines(lines)
-        print("[*] Value changed with success.")
-        with open(path, 'r') as save:
-            lines = save.readlines()
-        if lines[8] == 'weapon=0':
-            choice = input("Do you want to get the axe (weapon) ? [Y/N]")
-            if choice in ['y', 'Y']:
-                lines[8] = "weapon=1\n"
-                with open(path, 'w+') as save:
-                    save.writelines(lines)
+    # Verify if valid
+    if not directory:
+        print("\n[!] ERROR: Please select a directory.")
+        return
 
-    elif directory is None or directory == '':  # If the user didn't choose a file
-        print("\n[!] ERROR: Please select a repertory.")
-    else:  # Error save file doesnt exist
+    # Open the save file
+    path = Path(directory) / "save_data_main.ini"
+    if not path.is_file():
         print("\n[!] ERROR: Save file doesn't exist! Try to create a new Game.")
-    input("[*] Press Enter to continue...")
-    exit()
+        return
+    with open(path, 'r') as save_file:
+        lines = save_file.readlines()
 
+    # Changing room value
+    room = input("\n[#] Change the room value (0-999): ")
+    if not room.isdigit() or not 0 <= int(room) <= 999:
+        print("[!] ERROR: You can only enter an integer between 0 and 999.")
+        return
+
+    lines[5] = f"room={room}\n"
+    with open(path, 'w') as save_file:
+        save_file.writelines(lines)
+    print("[*] Value changed successfully.")
+
+    # Change axe weapon
+    if lines[8].strip() == 'weapon=0':
+        choice = input("Do you want to get the axe (weapon) ? [Y/N]")
+        if choice.lower() == 'y':
+            lines[8] = "weapon=1\n"
+            with open(path, 'w') as save_file:
+                save_file.writelines(lines)
 
 if __name__ == '__main__':
     main()
